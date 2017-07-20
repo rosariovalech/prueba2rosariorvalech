a4f3f342a95ce7531af6f183f81aab2ac89116a0
Rosario Valech Alessandri <valech.rosario@gmail.com>
2017-07-19 20:39:07 -0400

    quinto commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
 	belongs_to :category
 	belongs_to :user
+
+	scope :size_x, ->(x) { where(size: x) }
+	scope :without_user, includes(:user).where(:user => { :id => nil })
 end
